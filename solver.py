57ede43f71d672bbba3ba0ce4e0eca94e87ed038
Zgell <zgellner2001@gmail.com>
2020-09-17 09:40:28 -0600

    Further progress on the equation solver base

--- solver.py
+++ solver.py
@@ -6,6 +6,7 @@ def solve(eq):
     if (eq.count('(') > 0):  # IF there's more than one "level"...
     # Find the location of the first left bracket
     lbracket = eq.find('(')
+    rbracket = eq[::-1].find(')')
     # Find the location of the last right bracket
     # Recursively solve the equation inside the brackets
     pass
